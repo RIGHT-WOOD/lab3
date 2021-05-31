0fa6632845165b700d739cdc0828d0a5c9844204
RIGHT-WOOD <84662526+RIGHT-WOOD@users.noreply.github.com>
2021-06-01 00:53:34 +0800

    Initial commit

---
+++ code/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using ICMP.Network;
+
+namespace ICMP {
+	class Program {
+		[STAThread]
+		static void Main(string[] args) {
+			// Testing
+			Console.WriteLine($"{new string('-', 10)} Test ping {new string('-', 10)}\r\n");
+			var request = new IcmpRequest(new CmdRequestInfo("www.github.com", "Hello", CmdType.ping, 4, 32, 1000, 1000, 128));
+
+			Console.WriteLine($"\r\n\r\n{new string('-', 10)} Test tracert {new string('-', 10)}\r\n");
+			request = new IcmpRequest(new CmdRequestInfo("www.bmstu.ru", "Hello", CmdType.tracert, 4, 32, 1000, 1000, 128));
+
+            Console.WriteLine($"{new string('-', 10)} Test ping {new string('-', 10)}\r\n");
+            request = new IcmpRequest(new CmdRequestInfo("www.google.com", "Hello", CmdType.ping, 4, 32, 1000, 1000, 128));
+
+            Console.WriteLine($"{new string('-', 10)} Test ping {new string('-', 10)}\r\n");
+            request = new IcmpRequest(new CmdRequestInfo("www.rambler.com", "Hello", CmdType.ping, 4, 32, 1000, 1000, 128));
+
+            Console.WriteLine($"{new string('-', 10)} Test ping {new string('-', 10)}\r\n");
+            request = new IcmpRequest(new CmdRequestInfo("www.habr.com", "Hello", CmdType.ping, 4, 32, 1000, 1000, 128));
+
+            Console.WriteLine("\n\nPress any key to exit...");
+			Console.ReadLine();
+		}
+	}
+}
